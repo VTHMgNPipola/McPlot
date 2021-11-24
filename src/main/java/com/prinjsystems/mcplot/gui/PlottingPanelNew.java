be0006a2dfda47ba187b82a0f87eaad8c132dcc2
VTHMgNPipola <web.anunciante@gmail.com>
2021-11-24 17:19:16 -0300

    Texto nos paços maiores e cores configuráveis (futuramente) adicionadas.

--- src/main/java/com/prinjsystems/mcplot/gui/PlottingPanelNew.java
+++ src/main/java/com/prinjsystems/mcplot/gui/PlottingPanelNew.java
@@ -2,6 +2,8 @@ package com.prinjsystems.mcplot.gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
@@ -11,8 +13,16 @@ import javax.swing.JPanel;
 
 public class PlottingPanelNew extends JPanel {
     private int cameraX, cameraY;
-    private int scaleX, scaleY;
+    private final int scaleX = 1;
+    private final int scaleY = 1;
     private final int pixelsPerStep;
+
+    private final Font font;
+    private final Color backgroundColor = Color.white;
+    private final Color minorGridColor = new Color(232, 232, 232);
+    private final Color majorGridColor = Color.lightGray;
+    private final Color globalAxisColor = Color.black;
+    private FontMetrics fontMetrics;
 
     public PlottingPanelNew() {
         setPreferredSize(new Dimension(800, 600));
@@ -21,13 +31,15 @@ public class PlottingPanelNew extends JPanel {
 
         pixelsPerStep = 50;
 
+        font = new Font("Monospaced", Font.PLAIN, 12);
+
         final boolean[] dragging = new boolean[1];
         final int[] startPos = new int[2];
         // TODO: Start dragging when shift is pressed
         addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                if (e.getButton() == MouseEvent.BUTTON2 || e.getButton() == MouseEvent.BUTTON3) {
+                if (e.getButton() == MouseEvent.BUTTON2 || e.getButton() == MouseEvent.BUTTON1) {
                     dragging[0] = true;
                     startPos[0] = e.getXOnScreen();
                     startPos[1] = e.getYOnScreen();
@@ -36,7 +48,7 @@ public class PlottingPanelNew extends JPanel {
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                if (e.getButton() == MouseEvent.BUTTON2 || e.getButton() == MouseEvent.BUTTON3) {
+                if (e.getButton() == MouseEvent.BUTTON2 || e.getButton() == MouseEvent.BUTTON1) {
                     dragging[0] = false;
                 }
             }
@@ -73,16 +85,18 @@ public class PlottingPanelNew extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
         Graphics2D g = (Graphics2D) graphics;
+        g.setFont(font);
+        fontMetrics = g.getFontMetrics();
 
         // Background
-        g.setColor(Color.white);
+        g.setColor(backgroundColor);
         g.fillRect(0, 0, getWidth(), getHeight());
 
         // Camera translation
         g.translate(-cameraX, -cameraY);
 
         // Minor grid
-        g.setColor(new Color(232, 232, 232));
+        g.setColor(minorGridColor);
         for (int i = cameraX - (cameraX % (pixelsPerStep / 5)); i < cameraX + getWidth(); i += pixelsPerStep / 5) {
             g.drawLine(i, cameraY, i, cameraY + getHeight());
         }
@@ -91,18 +105,37 @@ public class PlottingPanelNew extends JPanel {
             g.drawLine(cameraX, i, cameraX + getWidth(), i);
         }
 
-        // Major grid
-        g.setColor(Color.lightGray);
+        // Major grid and steps
         for (int i = cameraX - (cameraX % pixelsPerStep); i < cameraX + getWidth(); i += pixelsPerStep) {
+            // Major grid
+            g.setColor(majorGridColor);
             g.drawLine(i, cameraY, i, cameraY + getHeight());
+
+            // Step
+            if (i != 0) {
+                g.setColor(globalAxisColor);
+                g.drawLine(i, -5, i, 5);
+                String step = ((i / pixelsPerStep) / scaleX) + "";
+                g.drawString(step, i - fontMetrics.stringWidth(step) / 2, 7 + fontMetrics.getAscent());
+            }
         }
 
         for (int i = cameraY - (cameraY % pixelsPerStep); i < cameraY + getHeight(); i += pixelsPerStep) {
+            // Major grid
+            g.setColor(majorGridColor);
             g.drawLine(cameraX, i, cameraX + getWidth(), i);
+
+            // Step
+            if (i != 0) {
+                g.setColor(globalAxisColor);
+                g.drawLine(-5, i, 5, i);
+                String step = ((i / pixelsPerStep) / scaleX) + "";
+                g.drawString(step, -7 - fontMetrics.stringWidth(step), i + fontMetrics.getAscent() / 2);
+            }
         }
 
         // Global axis
-        g.setColor(Color.black);
+        g.setColor(globalAxisColor);
         g.drawLine(cameraX, 0, cameraX + getWidth(), 0);
         g.drawLine(0, cameraY, 0, cameraY + getHeight());
     }
