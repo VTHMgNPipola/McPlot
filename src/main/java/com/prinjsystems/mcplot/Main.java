bd1db42613885e9e444fa3f2f14e7e3fc9d64516
VTHMgNPipola <web.anunciante@gmail.com>
2021-11-25 19:10:55 -0300

    Adicionado dependência do exp4j, que será o novo resolvedor de expressões (até eu escrever meu próprio compilador de expressões matemáticas para CPU e GPU, hehehe), e FlatLaf, que será uma nova opção de temas e o novo tema padrão.

--- src/main/java/com/prinjsystems/mcplot/Main.java
+++ src/main/java/com/prinjsystems/mcplot/Main.java
@@ -1,12 +1,12 @@
 package com.prinjsystems.mcplot;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import com.prinjsystems.mcplot.gui.WorkspaceSettings;
 import com.prinjsystems.mcplot.ngui.Workspace;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
     public static final ResourceBundle BUNDLE = ResourceBundle.getBundle("mcplot",
@@ -15,37 +15,13 @@ public class Main {
     public static final String PREFERENCES_PATH = "com.prinjsystems.mcplot";
 
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(WorkspaceSettings.getLookAndFeel());
-        } catch (IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-//        SwingUtilities.invokeLater(() -> {
-//            PlottingPanel plottingPanel = new PlottingPanel();
-//            JFrame frame = new JFrame("McPlot - New Plotting Panel Testing");
-//            frame.setContentPane(plottingPanel);
-//            frame.pack();
-//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//            frame.setLocationRelativeTo(null);
-//            frame.setVisible(true);
-//        });
+        FlatLightLaf.setup();
+        JFrame.setDefaultLookAndFeelDecorated(true);
 
         SwingUtilities.invokeLater(() -> {
             Workspace workspace = new Workspace();
             workspace.init();
             workspace.setVisible(true);
         });
-
-//        // Create function evaluator pool
-//        FunctionEvaluatorWorkerPool pool = new FunctionEvaluatorWorkerPool();
-//
-//        // Startup Swing
-//        SwingUtilities.invokeLater(() -> {
-//            Workspace workspace = Workspace.getInstance();
-//            workspace.setVisible(true);
-//            workspace.configure(args);
-//            PlottingPanel.getInstance().subscribeEvaluatorPool(pool);
-//        });
     }
 }
