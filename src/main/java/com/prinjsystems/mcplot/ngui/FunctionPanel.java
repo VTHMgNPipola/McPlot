87bb8e8d5c0cadce5444a48c170c4d60976b51d6
VTHMgNPipola <web.anunciante@gmail.com>
2021-11-26 13:50:42 -0300

    Adicionado botão para criar funções no painel de funções.

--- src/main/java/com/prinjsystems/mcplot/ngui/FunctionPanel.java
+++ src/main/java/com/prinjsystems/mcplot/ngui/FunctionPanel.java
@@ -2,8 +2,11 @@ package com.prinjsystems.mcplot.ngui;
 
 import com.prinjsystems.mcplot.ngui.components.FunctionCard;
 import java.util.concurrent.atomic.AtomicInteger;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
+
+import static com.prinjsystems.mcplot.Main.BUNDLE;
 
 public class FunctionPanel extends JPanel {
     private final AtomicInteger functionCount;
@@ -13,5 +16,13 @@ public class FunctionPanel extends JPanel {
         functionCount = new AtomicInteger(1);
 
         add(new FunctionCard(functionCount.getAndIncrement()), "pushx, span, growx");
+
+        JButton addFunctionCard = new JButton(BUNDLE.getString("workspace.actions.createFunction"));
+        add(addFunctionCard, "growx");
+        addFunctionCard.addActionListener(e -> {
+            add(new FunctionCard(functionCount.getAndIncrement()), "pushx, span, growx",
+                    getComponentCount() - 1);
+            updateUI();
+        });
     }
 }
