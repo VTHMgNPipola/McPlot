a6314227df6926faf7403d299575153dbcb42742
VTHMgNPipola <web.anunciante@gmail.com>
2021-11-27 14:44:16 -0300

    Adicionado submenu de exportação.

--- src/main/java/com/prinjsystems/mcplot/ngui/Workspace.java
+++ src/main/java/com/prinjsystems/mcplot/ngui/Workspace.java
@@ -58,6 +58,24 @@ public class Workspace extends JFrame {
 
         file.addSeparator();
 
+        // Export submenu
+        JMenu export = new JMenu(BUNDLE.getString("workspace.menu.file.export"));
+        file.add(export);
+
+        JMenuItem exportSpreadsheet = new JMenuItem(BUNDLE.getString("workspace.menu.file.export.spreadsheet"));
+        export.add(exportSpreadsheet);
+
+        JMenuItem exportText = new JMenuItem(BUNDLE.getString("workspace.menu.file.export.text"));
+        export.add(exportText);
+
+        JMenuItem exportPgfplots = new JMenuItem(BUNDLE.getString("workspace.menu.file.export.pgfplots"));
+        export.add(exportPgfplots);
+
+        JMenuItem exportPicture = new JMenuItem(BUNDLE.getString("workspace.menu.file.export.picture"));
+        export.add(exportPicture);
+
+        file.addSeparator();
+
         JMenuItem settings = new JMenuItem(BUNDLE.getString("generics.settings"));
         file.add(settings);
         settings.addActionListener(workspaceController::openSettings);
