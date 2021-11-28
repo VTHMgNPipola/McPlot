455926ef3a75e32a256d9a873162b27766a2163a
VTHMgNPipola <web.anunciante@gmail.com>
2021-11-27 23:21:38 -0300

    Added LICENSE.

--- src/main/java/com/prinjsystems/mcplot/ngui/AboutFrame.java
+++ src/main/java/com/prinjsystems/mcplot/ngui/AboutFrame.java
@@ -1,3 +1,21 @@
+/*
+ * McPlot - a reliable, powerful, lightweight and free graphing calculator
+ * Copyright (C) 2021  VTHMgNPipola
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.prinjsystems.mcplot.ngui;
 
 import java.awt.Font;
@@ -32,6 +50,9 @@ public class AboutFrame extends JFrame {
 
         JLabel description = new JLabel(BUNDLE.getString("about.description"));
         contentPane.add(description, "alignx center");
+
+        JLabel license = new JLabel(BUNDLE.getString("about.license"));
+        contentPane.add(license, "alignx center");
 
         JLabel version = new JLabel(MessageFormat.format(BUNDLE.getString("about.version"), VERSION));
         contentPane.add(version, "alignx center");
