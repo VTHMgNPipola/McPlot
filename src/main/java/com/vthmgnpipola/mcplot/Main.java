511f3e62008f91665339141f14552890b2a1d179
VTHMgNPipola <web.anunciante@gmail.com>
2021-11-28 00:05:18 -0300

    Release 0.1.

--- src/main/java/com/vthmgnpipola/mcplot/Main.java
+++ src/main/java/com/vthmgnpipola/mcplot/Main.java
@@ -34,7 +34,7 @@ public class Main {
 
     public static final String PREFERENCES_PATH = "com.vthmgnpipola.mcplot";
 
-    public static final String VERSION = "0.1-SNAPSHOT";
+    public static final String VERSION = "0.1";
 
     public static final ExecutorService EXECUTOR_THREAD = Executors.newSingleThreadExecutor();
 
