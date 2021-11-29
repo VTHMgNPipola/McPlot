bc1806da454ee65a9365431226d5d24509cbbe28
VTHMgNPipola <web.anunciante@gmail.com>
2021-11-29 11:56:26 -0300

    Started writing code documentation.

--- src/main/java/com/vthmgnpipola/mcplot/Main.java
+++ src/main/java/com/vthmgnpipola/mcplot/Main.java
@@ -27,17 +27,46 @@ import java.util.concurrent.Executors;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+/**
+ * McPlot is a lightweight graphing calculator written in Java 17, free for anyone to use.
+ * <p>
+ * My goal with McPlot is to create a reliable, powerful and lightweight graphing calculator that is also free and
+ * open-source, so that anyone can use it, even with the most basic of computers (provided it supports Java 17).
+ * <p>
+ * This class serves mainly as a bootstrapper for the AWT Event Dispatcher for the GUI and a single threaded executor
+ * service for general tasks.
+ */
 public class Main {
+    /**
+     * Resource bundle used to set the text values of GUI components depending on the select language
+     */
     public static final ResourceBundle BUNDLE = ResourceBundle.getBundle("mcplot",
             Locale.forLanguageTag(PreferencesHelper.PREFERENCES.get(PreferencesHelper.KEY_LANGUAGE,
                     Language.LANGUAGES[0].getTag())));
 
+    /**
+     * Path used in the Preferences object, on {@link PreferencesHelper}, to load and save settings
+     */
     public static final String PREFERENCES_PATH = "com.vthmgnpipola.mcplot";
 
+    /**
+     * McPlot version, displayed in the Workspace window title and About window.
+     */
     public static final String VERSION = "0.1";
 
+    /**
+     * Single threaded executor for general tasks done by the application. Normally used when a task might be too
+     * long to run on the AWT EventDispatcher
+     */
     public static final ExecutorService EXECUTOR_THREAD = Executors.newSingleThreadExecutor();
 
+    /**
+     * First method called by the JVM. Used to initialize the LookAndFeel and the AWT EventDispatcher to start the GUI.
+     * The AWT EventDispatcher then starts other services, such as the
+     * {@link com.vthmgnpipola.mcplot.nmath.MathEvaluatorPool}.
+     *
+     * @param args Command line arguments, which are ignored.
+     */
     public static void main(String[] args) {
         FlatLightLaf.setup();
         JFrame.setDefaultLookAndFeelDecorated(true);
