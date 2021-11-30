0252b9352b87abe7869aeb29ad908f05bf3c8a3e
VTHMgNPipola <web.anunciante@gmail.com>
2021-11-30 01:39:53 -0300

    Added documentation to FunctionEvaluator

--- src/main/java/com/vthmgnpipola/mcplot/nmath/FunctionEvaluator.java
+++ src/main/java/com/vthmgnpipola/mcplot/nmath/FunctionEvaluator.java
@@ -28,6 +28,12 @@ import java.util.stream.Collectors;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
+/**
+ * This class is responsible for the "glue logic" between the {@link Function} class, that only stores data about
+ * a function, the {@link MathEvaluatorPool} class, which is a thread pool that actually solves the expressions, and
+ * the {@link MathEventStreamer} class, which streams modifications done to any function or constant to every other
+ * {@code FunctionEvaluator} and {@link ConstantEvaluator}.
+ */
 public class FunctionEvaluator {
     private static final Pattern FUNCTION_PATTERN = Pattern.compile("\s*[a-zA-Z]+[a-zA-Z0-9]*\s*\\(\s*[a-zA-Z]+\s*\\)" +
             "\s*=[^=]*");
@@ -44,6 +50,14 @@ public class FunctionEvaluator {
     private final FunctionPlot plot;
     private Expression expression;
 
+    /**
+     * Creates a new FunctionEvaluator and binds it to a function, event streamer and plotting panel. Also registers
+     * two constant evaluators in the parent event streamer for the domain start and domain end values.
+     *
+     * @param function      Function this function evaluator "takes care" of.
+     * @param parent        Event streamer used when sending events.
+     * @param plottingPanel Plotting panel where the function will be plotted.
+     */
     public FunctionEvaluator(Function function, MathEventStreamer parent, PlottingPanel plottingPanel) {
         this.function = function;
         this.parent = parent;
@@ -68,29 +82,71 @@ public class FunctionEvaluator {
         parent.functionUpdate(true);
     }
 
+    /**
+     * Defines the definition string for the domain start constant. Doing so will trigger a constant update event on
+     * the event streamer bound to this function evaluator, which will recalculate every constant and function
+     * registered on the event streamer, effectively applying the new domain start value.
+     *
+     * @param domainStart Definition string of the domain start of the function.
+     */
     public void setDomainStart(String domainStart) {
         domainStartEvaluator.setDefinition(domainStart);
     }
 
+    /**
+     * Defines the definition string for the domain end constant. Doing so will trigger a constant update event on
+     * the event streamer bound to this function evaluator, which will recalculate every constant and function
+     * registered on the event streamer, effectively applying the new domain end value.
+     *
+     * @param domainEnd Definition string of the domain end of the function.
+     */
     public void setDomainEnd(String domainEnd) {
         domainEndEvaluator.setDefinition(domainEnd);
     }
 
+    /**
+     * Sets a new trace color on the function bound to this function evaluator and repaints the plotting panel
+     * (doesn't recalculate any functions or constants while doing so).
+     *
+     * @param traceColor New trace color.
+     */
     public void setTraceColor(Color traceColor) {
         function.setTraceColor(traceColor);
         plottingPanel.repaint();
     }
 
+    /**
+     * Defines if the function bound to this function evaluator will be filled by the plotting panel or not, and
+     * repaints it to apply the modification.
+     *
+     * @param filled Set to true if the function should be filled while rendering the graph in the plotting panel,
+     *               false otherwise.
+     */
     public void setFilled(boolean filled) {
         function.setFilled(filled);
         plottingPanel.repaint();
     }
 
+    /**
+     * Defines if the function bound to this function evaluator should be calculated and displayed on the plotting
+     * panel. If set to false, the function will never be recalculated or displayed on the plotting panel.
+     *
+     * @param visible Set to true if the function should be calculated and displayed, false otherwise.
+     */
     public void setVisible(boolean visible) {
         function.setVisible(visible);
         plottingPanel.repaint();
     }
 
+    /**
+     * This method takes care of determining the exact domain start, domain end and step size values, and submitting
+     * the function that is bound to this function evaluator to be calculated on the {@link MathEvaluatorPool} that
+     * was also bound to this function evaluator.
+     * <p>
+     * Note that the domain start and end constants aren't calculated at this step, instead this method simply checks
+     * if they are defined. If so, it uses their values, otherwise it calculates them based on the camera position,
+     * graph width and zoom.
+     */
     public void evaluate() {
         double zoomX = plottingPanel.getScaleX() * plottingPanel.getPixelsPerStep() * plottingPanel.getZoom();
         double domainStart = function.getDomainStart().getActualValue() != null ?
@@ -112,6 +168,14 @@ public class FunctionEvaluator {
                 });
     }
 
+    /**
+     * Processes a formation law into an exp4j's {@link Expression} object, using the constants defined on parent
+     * event streamer.
+     * <p>
+     * The formation law is determined using a private method, {@link #processFormationLaw()}, which will basically
+     * remove any calls to other functions defined on the parent event streamer and replace them with their formation
+     * laws, and do this until there are no more calls to a function defined on the event streamer.
+     */
     public void processExpression() {
         if (function == null || function.getDefinition() == null ||
                 !FUNCTION_PATTERN.matcher(function.getDefinition()).matches()) {
