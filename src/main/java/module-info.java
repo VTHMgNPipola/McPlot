16830af258167e88169c31f4ac3452c6cda02ae1
VTHMgNPipola <web.anunciante@gmail.com>
2021-12-28 14:38:07 -0300

    Added module-info. I still can't use jlink because some modules (FastCSV and some modules used by Apache POI I think) are automatic, but it's there if I figure it out.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module mcplot {
+    requires java.desktop;
+    requires java.prefs;
+    requires com.formdev.flatlaf;
+    requires exp4j;
+    requires com.miglayout.swing;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+    requires fastods;
+    requires de.siegmar.fastcsv;
+}
